9b1273171b22c01ea821715d6afadb44c5cf5a11
Justin Foster <54294620+fosterj096@users.noreply.github.com>
2019-12-29 17:39:20 -0600

    Update LeagueRankFinder.py

--- LeagueRankFinder.py
+++ LeagueRankFinder.py
@@ -1,13 +1,12 @@
-import bs4
-
+import bs4 #importing beautiful soup for parsing through html contents
 
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-#need to find way to tack on invidual user name after the original link
+#User input for summoner name, fixes space issues
 print("What is your summoner name?")
 noModName = input()
-modifiedName = noModName.replace(' ', '+')#replaces spaces with + for op.gg url
+modifiedName = noModName.replace(' ', '+')
 my_url = "https://na.op.gg/summoner/userName=" + modifiedName #saving url in variable
 
 #reads the url and information
@@ -15,10 +14,10 @@ uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
-#parses through html
+#parses through html and stores all raw html code in page_soup
 page_soup = soup(page_html, "html.parser")
 
-#finding the object we need in the html code from webpage
+#finding the object we need in the html code from op.gg.summonerName
 rank_container = page_soup.findAll("div", {"class":"TierRank"})
 rank = rank_container[0].text
 print("Your rank is", rank)
